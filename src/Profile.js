4c4c3adf83c55a2a36eb0886be9679246de04fb8
Xvois <sonnymparker9@gmail.com>
2022-10-12 21:11:09 +0100

    Stage 2.1

--- src/Profile.js
+++ src/Profile.js
@@ -7,7 +7,6 @@ const Profile = () => {
     const [username, setUsername] = useState(window.location.hash.split("#")[1]);
     const [profilePicture, setProfilePicture] = useState("");
     const [media, setMedia] = useState("")
-    const [mediaLink, setMediaLink] = useState("")
     useEffect(() => {
         if(username === "me"){ //if we are on the logged in user's page
             fetchData("me").then(function(result){ //get profile details
@@ -18,10 +17,9 @@ const Profile = () => {
                 let tempMedia = result.item.name + " -";
                 result.item.artists.forEach(function(element, i){ //add commas for songs with multiple artists
                     tempMedia +=  " " + element.name;
-                    if(i !== (result.item.artists).length - 1){ tempMedia += ","; } //stop if we are one before the end
+                    if(i !== (result.item.artists).length - 1){ tempMedia += ","; } //stop adding commas if we are one before the end
                 })
                 setMedia(tempMedia)
-                setMediaLink(result.item.external_urls.spotify)
             })
         }else{
             fetchData(`users/${username}`).then(function(result){ //if we are not, get their details
@@ -30,19 +28,22 @@ const Profile = () => {
             });
         }
         document.title = `Photon | ${username}`;
-    }, [profilePicture, username, media, mediaLink])
+    }, [profilePicture, username, media])
   return (
-      <div className='user-container'>
-            <img className='profile-picture' alt='Profile' src={profilePicture}></img>
-            <div className='text-container'>
-                <div className='username'>{username}</div>
-                {media ? 
-                <div className='currently-listening-media'>Currently listening to: <br></br>{media}</div>
-                :
-                <></>
-                }
-            </div>
-      </div>
+      <>
+        <div className='user-container'>
+                <img className='profile-picture' alt='Profile' src={profilePicture}></img>
+                <div className='text-container'>
+                    <div className='username'>{username}</div>
+                    {media ? 
+                    <div className='currently-listening-media'>Currently listening to: <br></br>{media}</div>
+                    :
+                    <></>
+                    }
+                </div>
+        </div>
+        <div className='simple-container'></div>
+      </>
   )
 }
 
