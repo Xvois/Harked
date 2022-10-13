cbcd8a62404bc2ceca68f5a9e93110337484bf82
Xvois <sonnymparker9@gmail.com>
2022-10-13 18:26:18 +0100

    Scope fix

--- src/Authentication.js
+++ src/Authentication.js
@@ -4,7 +4,7 @@ const CLIENT_ID = "a0b3f8d150d34dd79090608621999149";
 const REDIRECT_URI = "http://localhost:3000/authentication";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
-const SCOPES = "user-read-currently-playing, user-read-playback-state"
+const SCOPES = "user-read-currently-playing, user-read-playback-state, user-top-read"
 export const authURI = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPES}`;
 
 function Authentication(){
