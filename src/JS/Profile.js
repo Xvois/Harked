d6494247763a6b44fede6018105edf0d374dd923
Xvois <sonnymparker9@gmail.com>
2022-11-08 10:41:40 +0000

    New Focus

--- src/JS/Profile.js
+++ src/JS/Profile.js
@@ -139,7 +139,7 @@ const Profile = () => {
                             <div className='user-details'>
                                 <a href = {`https://open.spotify.com/user/${currentUser.userID}`} className='spotify-link'>
                                     <svg xmlns="http://www.w3.org/2000/svg" height="20px" width="20px" version="1.1" viewBox="0 0 168 168">
-                                        <path fill="white" d="m83.996 0.277c-46.249 0-83.743 37.493-83.743 83.742 0 46.251 37.494 83.741 83.743 83.741 46.254 0 83.744-37.49 83.744-83.741 0-46.246-37.49-83.738-83.745-83.738l0.001-0.004zm38.404 120.78c-1.5 2.46-4.72 3.24-7.18 1.73-19.662-12.01-44.414-14.73-73.564-8.07-2.809 0.64-5.609-1.12-6.249-3.93-0.643-2.81 1.11-5.61 3.926-6.25 31.9-7.291 59.263-4.15 81.337 9.34 2.46 1.51 3.24 4.72 1.73 7.18zm10.25-22.805c-1.89 3.075-5.91 4.045-8.98 2.155-22.51-13.839-56.823-17.846-83.448-9.764-3.453 1.043-7.1-0.903-8.148-4.35-1.04-3.453 0.907-7.093 4.354-8.143 30.413-9.228 68.222-4.758 94.072 11.127 3.07 1.89 4.04 5.91 2.15 8.976v-0.001zm0.88-23.744c-26.99-16.031-71.52-17.505-97.289-9.684-4.138 1.255-8.514-1.081-9.768-5.219-1.254-4.14 1.08-8.513 5.221-9.771 29.581-8.98 78.756-7.245 109.83 11.202 3.73 2.209 4.95 7.016 2.74 10.733-2.2 3.722-7.02 4.949-10.73 2.739z"/>
+                                        <path fill="black" d="m83.996 0.277c-46.249 0-83.743 37.493-83.743 83.742 0 46.251 37.494 83.741 83.743 83.741 46.254 0 83.744-37.49 83.744-83.741 0-46.246-37.49-83.738-83.745-83.738l0.001-0.004zm38.404 120.78c-1.5 2.46-4.72 3.24-7.18 1.73-19.662-12.01-44.414-14.73-73.564-8.07-2.809 0.64-5.609-1.12-6.249-3.93-0.643-2.81 1.11-5.61 3.926-6.25 31.9-7.291 59.263-4.15 81.337 9.34 2.46 1.51 3.24 4.72 1.73 7.18zm10.25-22.805c-1.89 3.075-5.91 4.045-8.98 2.155-22.51-13.839-56.823-17.846-83.448-9.764-3.453 1.043-7.1-0.903-8.148-4.35-1.04-3.453 0.907-7.093 4.354-8.143 30.413-9.228 68.222-4.758 94.072 11.127 3.07 1.89 4.04 5.91 2.15 8.976v-0.001zm0.88-23.744c-26.99-16.031-71.52-17.505-97.289-9.684-4.138 1.255-8.514-1.081-9.768-5.219-1.254-4.14 1.08-8.513 5.221-9.771 29.581-8.98 78.756-7.245 109.83 11.202 3.73 2.209 4.95 7.016 2.74 10.733-2.2 3.722-7.02 4.949-10.73 2.739z"/>
                                     </svg>
                                     <p>Open profile in Spotify.</p>
                                 </a>
@@ -147,31 +147,33 @@ const Profile = () => {
                                 <p>Playlists: PH</p>
                             </div>
                     </div>
+                    <h2 className='datapoint-title'>Top artists</h2>
                     <div className='simple-container'>
                         <div id='left-click'></div>
                         <div className='datapoint'>
-                            <h2 className='datapoint-title'>Top artists</h2>
                             <ol>
                                 <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[0], `${userID === "me" ? `Your top artist.` : `${currentUser.username}'s top artist.`}`)}>{datapoint.topArtists[0].name}</li>
-                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[1], `${userID === "me" ? `Your second to top artist.` : `${currentUser.username}'s second to top artist.`}`)}>{datapoint.topArtists[1].name}</li>
-                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[2], `${userID === "me" ? `Your third to top artist.` : `${currentUser.username}'s third to top artist.`}`)}>{datapoint.topArtists[2].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[1], `${userID === "me" ? `Your 2ⁿᵈ to top artist.` : `${currentUser.username}'s second to top artist.`}`)}>{datapoint.topArtists[1].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[2], `${userID === "me" ? `Your 3ʳᵈ to top artist.` : `${currentUser.username}'s third to top artist.`}`)}>{datapoint.topArtists[2].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[3], ``)}>{datapoint.topArtists[3].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[4], ``)}>{datapoint.topArtists[4].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[5], ``)}>{datapoint.topArtists[5].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[6], ``)}>{datapoint.topArtists[6].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[7], ``)}>{datapoint.topArtists[7].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[8], ``)}>{datapoint.topArtists[8].name}</li>
+                                <li className='list-item' onClick={() => updateFocus(datapoint.topArtists[9], ``)}>{datapoint.topArtists[9].name}</li>
                             </ol>
-                        </div>
                             <div className='art-container'>
-                                <a className={showArt ? 'play-wrapper' : '' } href={focus.link} rel="noopener noreferrer" target="_blank">
+                                <a className={showArt ? 'play-wrapper' : 'play-wrapper-hidden' } href={focus.link} rel="noopener noreferrer" target="_blank">
                                     <img className={showArt ? 'art-shown' : 'art-hidden' } src={focus.image} alt='Cover art'></img>
-                                </a>
-                                <div className='art-text-container'>
+                                    <div className='art-text-container'>
                                     <h1 className={showArt === "stick" ? "art-name-shown" : "art-name-hidden"}>{focus.title}</h1>
                                     <p className={showArt === "stick" ? "art-desc-shown" : "art-desc-hidden"} style={{fontSize: '40px'}}>{focus.secondary}</p>
                                     <p className={showArt === "stick" ? "art-desc-shown" : "art-desc-hidden"}>{focus.tertiary}</p>
-                                </div>
-                                {showArt?
-                                <button className='art-container-button' onClick={() => setShowArt(false)}>Hide</button>
-                                    :
-                                <></>
-                                }
+                                    </div>
+                                </a>
                             </div>
+                        </div>
                             <div id='right-click'></div>
                         </div>
                     {graph}
