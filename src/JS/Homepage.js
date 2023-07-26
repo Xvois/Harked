a957126d8ad10f43e5fd860a6a0ccd9321a21763
sonny <sonnymparker9@gmail.com>
2023-07-26 15:53:49 +0100

    1.4.4b Add back homepage!

--- src/JS/Homepage.js
+++ src/JS/Homepage.js
@@ -6,13 +6,12 @@
  */
 
 import {isLoggedIn, validateUser} from './HDM.ts';
-import React, {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import './../CSS/Homepage.css';
 import {handleAlternateLogin} from "./Authentication";
 
 function Homepage() {
 
-    const [loggedIn, setLoggedIn] = useState(isLoggedIn());
 
     useEffect(() => {
         document.title = "Harked"
@@ -22,29 +21,35 @@ function Homepage() {
 
     const handleLogOut = () => {
         window.localStorage.clear();
-        setLoggedIn(false);
     }
+
+    let exploreMessage = "Begin by exploring your own profile from a new perspective, or maybe discovering how you compare to others? It's your choice.";
+    let welcomeMessage = "Just click log-in to get started exploring your Spotify profile in a new light. None of your log-in information is shared with us.";
 
     return (
         <div className='homepage-container'>
             <div className='top-container'>
-                {loggedIn ?
+                {isLoggedIn() ?
                     <h1 className="main-text">Welcome.</h1>
                     :
                     <h1 className="main-text">Harked</h1>
                 }
+                <p className='under-text'>{isLoggedIn() ? exploreMessage : welcomeMessage}</p>
                 <div className={'button-wrapper'}>
-                    {!loggedIn ?
+                    {!isLoggedIn() ?
                         <>
-                            <button className="std-button" onClick={handleAlternateLogin}>Login with Spotify</button>
-                            <a className="std-button" href={'/profile/sonn-gb'}>View a sample profile</a>
+                            <button className="subtle-button" onClick={handleAlternateLogin}>Login with Spotify</button>
+                            <a className="subtle-button" href={'/profile/sonn-gb'}>View a sample profile</a>
                         </>
                         :
                         <>
-                            <a className={'std-button'} href={"/profile/me"}>Explore your profile</a>
-                            <a className={'std-button'} href={"/reviews/me"}>[Beta] Reviews</a>
-                            <button className={"std-button"} onClick={handleLogOut}>Log-out</button>
+                            <a className="subtle-button" href={`profile/me`}>Explore your profile</a>
                         </>
+                    }
+                    {isLoggedIn() ?
+                        <button className={"subtle-button"} onClick={handleLogOut}>Log-out</button>
+                        :
+                        <></>
                     }
                 </div>
                 <p style={{fontFamily: 'Inter Tight', marginTop: '20px', fontSize: '10px'}}>
